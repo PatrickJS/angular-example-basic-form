2671e628389ef7368311f96527af470b15bfe8d5
PatrickJS <github@gdi2290.com>
2015-03-03 22:52:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
-// Code goes here
-angular.module('App', ['ngMessages'])
-  .controller('LoginController', function($scope, Auth, $window) {
+angular.module('App', [
+  'ngMessages',
+  'fake-server'// ignore me 
+])
+.controller('LoginController', function($scope, Auth, $window) {
 
-    $scope.loading = false;
+  $scope.loading = false;
+  $scope.user = {
+    email: null,
+    password: null
+  };
+  
+  $scope.reset = function(form) {
     $scope.user = {
       email: null,
       password: null
     };
-    
-    $scope.reset = function(form) {
-      $scope.user = {
-        email: null,
-        password: null
-      };
-      $scope.confirmPassword = null
-      form.$setPristine();
-      form.$setUntouched();
-      // form reset hacks
-      form.$valid = true;
-      form.$invalid = false;
-      form.$error = {};
+    $scope.confirmPassword = null
+    form.$setPristine();
+    form.$setUntouched();
+    form.$valid = true;
+    form.$invalid = false;
+    form.$error = {};
+  }
+
+  $scope.submit = function(user, form) {
+    $scope.loading = true;
+    Auth.signup(user)
+    .then(function(data) {
+      $scope.reset(form);
+      console.log('User Login', user);
+      // redirect etc
+      // $state.go('app.home')
+    })
+    .catch(function(err) {
+      $window.alert(err);
+    })
+    .finally(function() {
       $scope.loading = false;
-    }
+    });
+  };
 
-    $scope.submit = function(user, form) {
-      $scope.loading = true;
-      Auth.signup(user)
-      .then(function(data) {
-        console.log('User Login', user);
-        // redirect etc
-        // $state.go('app.home')
+})
+.factory('Auth', function($q, $http) {
+  var authenticated = false;
+  return {
+    isAuthenticated: function() {
+      return authenticated;
+    },
+    signup: function(user) {
+      return $http.post('/api/signup', {
+        email: user.email,
+        password: user.password
       })
-      .catch(function(err) {
-        $window.alert(err);
+      .then(function(res) {
+        // do something here
+        authenticated = true;
+        return res.data;
       })
-      .finally(function() {
-        $scope.reset(form);
+      .catch(function(res) {
+        throw res.data;
       });
-    };
+    }
+  };
+})
+.directive('matchPassword', function() {
+  return {
+    restrict: 'A',
+    require: ['^ngModel', '^form'],
+    link: function(scope, element, attrs, ctrls) {
+      var formController = ctrls[1];
+      var ngModel = ctrls[0];
+      var otherPasswordModel = formController[attrs.matchPassword];
+  
+      ngModel.$validators.passwordMatch = function(modelValue) {
+        return (modelValue === otherPasswordModel.$modelValue);
+      };
 
-  })
-  .factory('Auth', function($q, $http) {
-    var authenticated = false;
-    return {
-      isAuthenticated: function() {
-        return authenticated;
-      },
-      signup: function(user) {
-        return $http.post('/api/signup', {
-          email: user.email,
-          password: user.password
-        })
-        .then(function(res) {
-          // do something here
-          authenticated = true;
-          return res.data;
-        })
-        .catch(function(res) {
-          throw res.data;
-        });
-      }
-    };
-  })
-  .directive('matchPassword', function() {
-    return {
-      restrict: 'A',
-      require: ['^ngModel', '^form'],
-      link: function(scope, element, attrs, ctrls) {
-        var formController = ctrls[1];
-        var ngModel = ctrls[0];
-        var otherPasswordModel = formController[attrs.matchPassword];
-    
-        ngModel.$validators.passwordMatch = function(modelValue) {
-          return (modelValue === otherPasswordModel.$modelValue);
-        };
-
-      } // end link
-    }; // end return
-  });
+    } // end link
+  }; // end return
+});
